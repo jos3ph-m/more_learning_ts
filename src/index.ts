e887136c2076243fcea332ac7cecffa50ffc363e
Joseph M <jmedina1985@hotmail.com>
2023-01-03 22:22:37 -0800

    TS classes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 class User {
   public email: string
   // everything is public by default
-  name: string
-  private readonly city: string = ""
+  private name: string
+  readonly city: string = ""
   constructor(email: string, name: string){
     this.email = email;
     this.name = name;
