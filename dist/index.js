032cfdc47fec7b21a256eb81bd34fea6255b98cf
Joseph M <jmedina1985@hotmail.com>
2023-01-05 22:14:21 -0800

    Why interface is so important

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,45 @@
 "use strict";
-console.log("typescript is here");
+// class User {
+//   // everything is public by default
+//   public email: string
+//   private name: string
+//   readonly city: string = ""
+//   constructor(email: string, name: string){
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "Los Angeles";
+    }
+    deleteToken() {
+        console.log("token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+const joey = new User("j@jm.com", "joey");
+// joey.city
