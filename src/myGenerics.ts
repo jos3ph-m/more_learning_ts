bfaf30cfaf86e2d00b3b92876f5d6fbfff47bfb2
Joseph M <jmedina1985@hotmail.com>
2023-01-08 10:40:29 -0800

    generics

--- src/myGenerics.ts
+++ src/myGenerics.ts
@@ -1,14 +1,21 @@
 const score: Array<number> = []
 const names: Array<string> = []
 
+
+// this could be anything
 function identityOne(val: boolean | number): boolean | number {
   return val
 }
 
+// this could be anything
 function identityTwo(val: any):any{
   return val
 }
 
+// once we pass on something that value type is logged.  so it cannot be any.
 function identityThree<Type>(val: Type): Type {
   return val
 }
+
+// for example
+identityThree(3)
