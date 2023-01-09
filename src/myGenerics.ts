333c4df81793f18eb57b477215d54e524fffc145
Joseph M <jmedina1985@hotmail.com>
2023-01-09 14:10:04 -0800

    Generics in array and arrow functions

--- src/myGenerics.ts
+++ src/myGenerics.ts
@@ -37,3 +37,7 @@ function getSearchProducts<T>(products: T[]): T {
   const myIndex = 3
   return products[myIndex]
 }
+
+const getMoreSearchProducts = <T>(products: T[]): T => {
+  // do some database operations
+}
